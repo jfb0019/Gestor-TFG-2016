5994da4b92b874088f2caacfff63c3742ce178f1
Javier de la Fuente Barrios <jfb0019@alu.ubu.es>
2016-10-09 22:53:28 +0200

    #3 Creado proyecto Vaadin nuevo.

---
+++ sistinf/src/main/java/ubu/digit/ui/SistInfUI.java
@@ -0,0 +1,59 @@
+package ubu.digit.ui;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("sistinftheme")
+public class SistInfUI extends UI {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -4568743602891945769L;
+
+	@Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+
+        Button button = new Button("Click Me");
+        button.addClickListener( e -> {
+            layout.addComponent(new Label("Thanks " + name.getValue() 
+                    + ", it works!"));
+        });
+        
+        layout.addComponents(name, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "SistInfUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = SistInfUI.class, productionMode = false)
+    public static class SistInfUIServlet extends VaadinServlet {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -8278292941976902830L;
+    }
+}
