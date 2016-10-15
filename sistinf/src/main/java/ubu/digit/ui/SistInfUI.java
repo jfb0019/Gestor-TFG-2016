ae7a26cec57a8f98a3187d3fc3d55851c0a3ba8a
Javier de la Fuente Barrios <jfb0019@alu.ubu.es>
2016-10-15 21:13:43 +0200

    #7 Añadido el código base para obtener los datos del tribunal.

--- sistinf/src/main/java/ubu/digit/ui/SistInfUI.java
+++ sistinf/src/main/java/ubu/digit/ui/SistInfUI.java
@@ -1,59 +1,67 @@
 package ubu.digit.ui;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import ubu.digit.pesistence.SistInfData;
+
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of a html page where a Vaadin application is
+ * embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
 @Theme("sistinftheme")
 public class SistInfUI extends UI {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4568743602891945769L;
 
 	@Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
+		SistInfData fachadaDatos = SistInfData.getInstance();
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        layout.setMargin(true);
-        layout.setSpacing(true);
-        
-        setContent(layout);
-    }
+		layout.addComponent(new Label("Tribunal:"));
+		try {
+			ResultSet result = fachadaDatos.getResultSet("Tribunal", "NombreApellidos");
+			while (result.next()) {
+				String cargo = result.getString("Cargo");
+				String nombre = result.getString("NombreApellidos");
+				layout.addComponent(new Label(cargo + ": " + nombre));
+			} // while
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		layout.setMargin(true);
+		layout.setSpacing(true);
+		setContent(layout);
+	}
 
-    @WebServlet(urlPatterns = "/*", name = "SistInfUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = SistInfUI.class, productionMode = false)
-    public static class SistInfUIServlet extends VaadinServlet {
+	@WebServlet(urlPatterns = "/*", name = "SistInfUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = SistInfUI.class, productionMode = false)
+	public static class SistInfUIServlet extends VaadinServlet {
 
 		/**
 		 * 
 		 */
 		private static final long serialVersionUID = -8278292941976902830L;
-    }
+	}
 }
