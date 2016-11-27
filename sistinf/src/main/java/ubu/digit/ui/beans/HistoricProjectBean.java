80b76de65c8f3a699f8bf5340d2060c4a5008132
Javier de la Fuente Barrios <jfb0019@alu.ubu.es>
2016-11-27 18:58:13 +0100

    #14 Añadido modelo de datos de los proyectos históricos.
    Cada proyecto está encapsulado en un JavaBean HistoricProjectBean.
    
    Eliminado los ficheros .csv correspondientes a la página de métricas.

---
+++ sistinf/src/main/java/ubu/digit/ui/beans/HistoricProjectBean.java
@@ -0,0 +1,161 @@
+package ubu.digit.ui.beans;
+
+import java.io.Serializable;
+
+public class HistoricProjectBean implements Serializable {
+
+	private static final long serialVersionUID = -4441566584690195452L;
+
+	private String title;
+
+	private String description;
+
+	private String tutor1;
+
+	private String tutor2;
+
+	private String tutor3;
+
+	private String student1;
+
+	private String student2;
+
+	private String student3;
+
+	private int numStudents;
+
+	private String assignmentDate;
+
+	private String presentationDate;
+
+	private String mark;
+
+	private int totalDays;
+
+	public HistoricProjectBean() {
+
+	}
+
+	public HistoricProjectBean(String title, String description, String tutor1, String tutor2, String tutor3,
+			String student1, String student2, String student3, int numStudents, String assignmentDate,
+			String presentationDate, String mark, int totalDays) {
+		this.title = title;
+		this.description = description;
+		this.tutor1 = tutor1;
+		this.tutor2 = tutor2;
+		this.tutor3 = tutor3;
+		this.student1 = student1;
+		this.student2 = student2;
+		this.student3 = student3;
+		this.numStudents = numStudents;
+		this.assignmentDate = assignmentDate;
+		this.presentationDate = presentationDate;
+		this.mark = mark;
+		this.totalDays = totalDays;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTutor1() {
+		return tutor1;
+	}
+
+	public void setTutor1(String tutor1) {
+		this.tutor1 = tutor1;
+	}
+
+	public String getTutor2() {
+		return tutor2;
+	}
+
+	public void setTutor2(String tutor2) {
+		this.tutor2 = tutor2;
+	}
+
+	public String getTutor3() {
+		return tutor3;
+	}
+
+	public void setTutor3(String tutor3) {
+		this.tutor3 = tutor3;
+	}
+
+	public String getStudent1() {
+		return student1;
+	}
+
+	public void setStudent1(String student1) {
+		this.student1 = student1;
+	}
+
+	public String getStudent2() {
+		return student2;
+	}
+
+	public void setStudent2(String student2) {
+		this.student2 = student2;
+	}
+
+	public String getStudent3() {
+		return student3;
+	}
+
+	public void setStudent3(String student3) {
+		this.student3 = student3;
+	}
+
+	public int getNumStudents() {
+		return numStudents;
+	}
+
+	public void setNumStudents(int numStudents) {
+		this.numStudents = numStudents;
+	}
+
+	public String getAssignmentDate() {
+		return assignmentDate;
+	}
+
+	public void setAssignmentDate(String assignmentDate) {
+		this.assignmentDate = assignmentDate;
+	}
+
+	public String getPresentationDate() {
+		return presentationDate;
+	}
+
+	public void setPresentationDate(String presentationDate) {
+		this.presentationDate = presentationDate;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public int getTotalDays() {
+		return totalDays;
+	}
+
+	public void setTotalDays(int totalDays) {
+		this.totalDays = totalDays;
+	}
+
+}
--- sistinf/src/main/java/ubu/digit/ui/views/HistoricProjectsView.java
+++ sistinf/src/main/java/ubu/digit/ui/views/HistoricProjectsView.java
@@ -1,9 +1,17 @@
 package ubu.digit.ui.views;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.VerticalLayout;
 
+import ubu.digit.pesistence.SistInfData;
+import ubu.digit.ui.beans.HistoricProjectBean;
 import ubu.digit.ui.components.Footer;
 import ubu.digit.ui.components.NavigationBar;
 
@@ -11,16 +19,78 @@ public class HistoricProjectsView extends VerticalLayout implements View {
 
 	private static final long serialVersionUID = 8431807779365780674L;
 
+	/**
+	 * Logger de la clase.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(HistoricProjectsView.class);
+
 	public static final String VIEW_NAME = "historic-projects";
 
+	private BeanItemContainer<HistoricProjectBean> beans;
+
+	private SistInfData fachadaDatos;
+
 	public HistoricProjectsView() {
+		fachadaDatos = SistInfData.getInstance();
 		setMargin(true);
 		setSpacing(true);
 		NavigationBar navBar = new NavigationBar();
 		addComponent(navBar);
 
+		createDataModel();
+		createMetrics();
+		createHistoricProjectsTable();
+
 		Footer footer = new Footer();
 		addComponent(footer);
+	}
+
+	private void createDataModel() {
+		beans = new BeanItemContainer<HistoricProjectBean>(HistoricProjectBean.class);
+		try (ResultSet result = fachadaDatos.getResultSet("Historico", "Titulo")) {
+			while (result.next()) {
+				int numStudents = 1;
+				String title = result.getString("Titulo");
+				String description = result.getString("Descripcion");
+				String tutor1 = result.getString("Tutor1");
+				String tutor2 = result.getString("Tutor2");
+				if (tutor2 == null)
+					tutor2 = "";
+				String tutor3 = result.getString("Tutor3");
+				if (tutor3 == null)
+					tutor3 = "";
+				String student1 = result.getString("Alumno1");
+				String student2 = result.getString("Alumno2");
+				if (student2 == null)
+					student2 = "";
+				else
+					numStudents++;
+				String student3 = result.getString("Alumno3");
+				if (student3 == null)
+					student3 = "";
+				else
+					numStudents++;
+				String assignmentDate = result.getString("FechaAsignacion");
+				String presentationDate = result.getString("FechaPresentacion");
+				String score = result.getString("Nota");
+				int totalDays = result.getShort("TotalDias");
+
+				HistoricProjectBean bean = new HistoricProjectBean(title, description, tutor1, tutor2, tutor3, student1,
+						student2, student3, numStudents, assignmentDate, presentationDate, score, totalDays);
+				beans.addBean(bean);
+			}
+		} catch (SQLException e) {
+			LOGGER.error("Error en históricos", e);
+		}
+
+	}
+
+	private void createMetrics() {
+
+	}
+
+	private void createHistoricProjectsTable() {
+
 	}
 
 	@Override
