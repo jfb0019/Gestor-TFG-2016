a3ce5bd8676745e8ba6541d11b149211c0228ab9
Javier de la Fuente Barrios <jfb0019@alu.ubu.es>
2016-12-01 12:10:34 +0100

    #15 Añadida imagen que enlaza al servidor de SonarQube.

--- sistinf/src/main/java/ubu/digit/ui/views/MetricsView.java
+++ sistinf/src/main/java/ubu/digit/ui/views/MetricsView.java
@@ -2,10 +2,14 @@ package ubu.digit.ui.views;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.ThemeResource;
+import com.vaadin.ui.Link;
 import com.vaadin.ui.VerticalLayout;
 
 import ubu.digit.ui.components.Footer;
 import ubu.digit.ui.components.NavigationBar;
+import ubu.digit.util.ExternalProperties;
 
 public class MetricsView extends VerticalLayout implements View {
 
@@ -13,16 +17,29 @@ public class MetricsView extends VerticalLayout implements View {
 
 	public static final String VIEW_NAME = "metrics";
 
+	private ExternalProperties config;
+
 	public MetricsView() {
+		config = ExternalProperties.getInstance("/WEB-INF/classes/config.properties", false);
 		setMargin(true);
 		setSpacing(true);
 		NavigationBar navBar = new NavigationBar();
 		addComponent(navBar);
 
+		addSonarImageLink();
+		
 		Footer footer = new Footer();
 		addComponent(footer);
 	}
 
+	private void addSonarImageLink(){
+		String urlSonar = config.getSetting("urlSonar");
+		Link sonarLink = new Link(null, new ExternalResource("https://" + urlSonar));
+		sonarLink.setIcon(new ThemeResource("img/tfgsonar.png"));
+		sonarLink.setTargetName("_blank");
+		addComponent(sonarLink);
+	}
+	
 	@Override
 	public void enter(ViewChangeEvent event) {
 	}
